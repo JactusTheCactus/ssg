34397a200f534efd6a8c49653a8139a20d117a4a
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-12-16 00:53:10 -0800

    -

--- .sh
+++ .sh
@@ -15,14 +15,9 @@ if flag local
 fi
 while read -r f
 	do yml "$f" -p yaml -o json | jq -c "." > "${f%yml}json"
-done < <(find . -name "*.yml" ! \( \
-	-path "./node_modules/*" \
-	-or -name "scripts.yml" \
-\))
+done < <(find . -name "*.yml" ! \( -path "./node_modules/*" -or -name "scripts.yml" \))
 while read -r f
-	do npx sass "$f:${f%scss}css" \
-		--no-source-map \
-		--style=compressed
+	do npx sass "$f:${f%scss}css" --no-source-map --style=compressed
 done < <(find . -name "*.scss")
 tsc
 node scripts/build.js
