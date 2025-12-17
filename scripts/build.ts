e34cfcbbc875ff785e653746791b81ce2a376d24
JactusTheCactus <threateninglyamusing@gmail.com>
2025-12-17 03:05:18 -0800

    Update build.ts

--- scripts/build.ts
+++ scripts/build.ts
@@ -24,13 +24,7 @@ glob("**/*.ejs", { cwd: path.join(src, "pages") })
 				)
 				.then((layout) => {
 					fse.writeFile(
-						path.join(
-							dest,
-								[
-									data.name,
-									"html"
-								].join(".")
-							),
+						path.join(dest, data.name + ".html"),
 						layout
 					);
 				})
