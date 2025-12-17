3aa39e13a72366c576a4f514cb5a08ef983789b3
JactusTheCactus <threateninglyamusing@gmail.com>
2025-12-17 02:57:55 -0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { readFile } from "node:fs/promises";
 export default {
 	site: {
 		title: "Characters",
-		description: "Micro Static Site Generator in Node.js",
+		description: "Some of my OCs",
 		chars: JSON.parse(
 			await readFile("./src/data/characters.json", "utf-8")
 		),
