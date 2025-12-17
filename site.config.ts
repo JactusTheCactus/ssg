c8cb75128a0a4ba82da398a7e60e2673dbcf2830
JactusTheCactus <threateninglyamusing@gmail.com>
2025-12-17 03:01:05 -0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,1 +1,10 @@
-import{readFile}from"node:fs/promises";export default{site:{title:"Characters",description:"Some of my OCs",chars:JSON.parse(await readFile("./src/data/characters.json","utf-8"))}}
+import {readFile} from "node:fs/promises"
+export default {
+	site: {
+		title: "Characters",
+		description: "Some of my OCs",
+		chars: JSON.parse(
+			await readFile("./src/data/characters.json", "utf-8")
+		)
+	}
+}
